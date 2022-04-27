702cc75cb92c29c45ea64e30bee804a413ea3dca
Robertino Bonora <robertinobonora@MacBook-Pro-de-Robertino.local>
2022-04-27 17:05:54 -0400

    Nuevo commit del proyecto poc-s3-glue-spectrum-dbt-redshift

--- macros/ejecucion_pre_modelo.sql
+++ macros/ejecucion_pre_modelo.sql
@@ -2,7 +2,7 @@
 {%- if this.table = '"ptlf"'-%}
 insert into dev.modelo_metricas.ejecucion_metrica (id_proceso,id_metrica,id_tipo_registro,tms_ejecucion_fin,val_metrica)
 values(1,NULL,1,NULL,NULL); commit;
-{%- elif  this.table = '"comercios"' -%}
+{%- elif this.table = '"comercios"' -%}
 insert into dev.modelo_metricas.ejecucion_metrica (id_proceso,id_metrica,id_tipo_registro,tms_ejecucion_fin,val_metrica)
 values(2,NULL,1,NULL,NULL); commit;
 {%- endif -%}
