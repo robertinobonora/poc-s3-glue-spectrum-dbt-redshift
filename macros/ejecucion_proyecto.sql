06b65d398ed82594b27cc0336e74e1ee914cd55d
Robertino Bonora <robertinobonora@MacBook-Pro-de-Robertino.local>
2022-04-26 21:25:59 -0400

    Primer commit poc-s3-glue-spectrum-dbt-redshift

--- macros/ejecucion_proyecto.sql
+++ macros/ejecucion_proyecto.sql
@@ -4,6 +4,6 @@ insert into dev.modelo_metricas.ejecucion_metrica (id_proceso,id_metrica,id_tipo
 values(NULL,NULL,'{{ tipo_registro }}',NULL,NULL); commit;
 {%- else -%}
 insert into dev.modelo_metricas.ejecucion_metrica (id_proceso,id_metrica,id_tipo_registro,tms_ejecucion_fin,val_metrica)
-values(NULL,NULL,'{{ tipo_registro }}','{{ dbt_utils.current_timestamp}}',NULL); commit;
+values(NULL,NULL,'{{ tipo_registro }}','{{ dbt_utils.current_timestamp }}',NULL); commit;
 {%- endif -%}
 {%- endmacro%}
